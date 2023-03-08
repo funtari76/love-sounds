6d4afd98cb5f9799d4879be2e4f39140ede264b1
funtari76 <funtari777@gmail.com>
2023-03-08 07:33:24 +0000

    changed the index styles and script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,61 +1,31 @@
-const questions = [{
-        question: "What is the frequency of a sound wave with a wavelength of 1 meter?",
-        answers: [{
-                text: "1 Hz",
-                correct: true
-            },
-            {
-                text: "2 Hz",
-                correct: false
-            },
-            {
-                text: "3 Hz",
-                correct: false
-            },
-            {
-                text: "4 Hz",
-                correct: false
-            },
-        ],
-    },
-    {
-        question: "What is the frequency of a sound wave with a wavelength of 2 meters?",
-        answers: [{
-                text: "1 Hz",
-                correct: false
-            },
-            {
-                text: "2 Hz",
-                correct: true
-            },
-            {
-                text: "3 Hz",
-                correct: false
-            },
-            {
-                text: "4 Hz",
-                correct: false
-            },
-        ],
-    },
-    {
-        question: "What is the frequency of a sound wave with a wavelength of 3 meters?",
-        answers: [{
-                text: "1 Hz",
-                correct: false
-            },
-            {
-                text: "2 Hz",
-                correct: false
-            },
-            {
-                text: "3 Hz",
-                correct: true
-            },
-            {
-                text: "4 Hz",
-                correct: false
-            },
-        ],
-    },
-]
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
